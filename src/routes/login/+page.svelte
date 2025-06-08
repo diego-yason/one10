af08de3b26c633b60605448a80039a1487e1709b
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-06-08 21:19:08 +0800

    added most of the login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,30 @@
-<button>You click this to login.</button>
+<!-- <div class="text-white"> -->
+<!-- Use the div with class once there is an image -->
+<div class="mx-auto w-96">
+	<!-- TODO: add 110 logo -->
+	<!-- TODO: add "Studio" outline -->
+	<span class="text-4xl font-spaceGrotesk font-bold">
+		<img src="https://placehold.co/60x50" alt="" class="inline" /> <span class="">Studio</span> Lab
+	</span>
+
+	<div>
+		<p class="text-xs">Welcome back</p>
+		<h1 class="font-spaceGrotesk text-3xl font-bold">Login to your account</h1>
+	</div>
+	<form action="" class="flex flex-col gap-6">
+		<input type="email" name="" id="" placeholder="Please enter your email" />
+		<input type="password" name="" id="" placeholder="Enter password" />
+		<button class="bg-amber-600 text-white">Login</button>
+	</form>
+	<a href="#">Forgot password?</a>
+
+	<div>
+		<p>Or login using</p>
+		<div class="flex gap-2.5">
+			<button class="p-2">G</button>
+			<button class="p-2">F</button>
+		</div>
+	</div>
+
+	<p>Not registered? <a href="#" class="bg">Create an account</a></p>
+</div>
