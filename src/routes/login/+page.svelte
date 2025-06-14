60cf2d7d0ea7cc616e2fd18955a745dd942af467
Lance Reyes <lr.lancereyes@gmail.com>
2025-06-14 17:29:01 +0800

    [FEAT] Register page (#5)
    * added background and centered div
    
    * finished implementing design
    
    * fixed button
    
    * Add register page
    
    ---------
    
    Co-authored-by: diego-yason <61825575+diego-yason@users.noreply.github.com>

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,30 +1,61 @@
 <!-- <div class="text-white"> -->
 <!-- Use the div with class once there is an image -->
-<div class="mx-auto w-96">
+<div class="mx-auto w-96 text-gray-100">
 	<!-- TODO: add 110 logo -->
 	<!-- TODO: add "Studio" outline -->
 	<span class="text-4xl font-spaceGrotesk font-bold">
-		<img src="https://placehold.co/60x50" alt="" class="inline" /> <span class="">Studio</span> Lab
+		<img src="https://placehold.co/60x50" alt="" class="inline" />
+		<span class="logoOutline text-transparent">Studio</span> Lab
 	</span>
 
-	<div>
+	<div class="mb-7 mt-20">
 		<p class="text-xs">Welcome back</p>
 		<h1 class="font-spaceGrotesk text-3xl font-bold">Login to your account</h1>
 	</div>
-	<form action="" class="flex flex-col gap-6">
-		<input type="email" name="" id="" placeholder="Please enter your email" />
-		<input type="password" name="" id="" placeholder="Enter password" />
-		<button class="bg-amber-600 text-white">Login</button>
+	<form action="" class="flex flex-col gap-6 mb-5">
+		<input
+			type="email"
+			name=""
+			id=""
+			placeholder="Please enter your email"
+			class="bg-white p-3 text-gray-800"
+		/>
+		<input
+			type="password"
+			name=""
+			id=""
+			placeholder="Enter password"
+			class="bg-white p-3 text-gray-800"
+		/>
+		<button class="bg-amber-600 text-white py-2">Login</button>
 	</form>
-	<a href="#">Forgot password?</a>
+	<a href="#" class="text-[#F2C94C] underline">Forgot password?</a>
 
-	<div>
+	<div class="mt-16 mb-11">
 		<p>Or login using</p>
 		<div class="flex gap-2.5">
-			<button class="p-2">G</button>
-			<button class="p-2">F</button>
+			<button class=""><img src="https://placehold.co/40" alt="" /></button>
+			<button class=""><img src="https://placehold.co/40" alt="" /></button>
 		</div>
 	</div>
 
-	<p>Not registered? <a href="#" class="bg">Create an account</a></p>
+	<p>Not registered? <a href="/register" class="text-brand underline">Create an account</a></p>
 </div>
+
+<style lang="postcss">
+	:global(body) {
+		background-image: url('https://www.pbs.org/wnet/nature/files/2021/01/pexels-denis-linine-714258.png');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		height: 100vh;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.logoOutline {
+		-webkit-text-stroke: 1px #fff;
+	}
+</style>
