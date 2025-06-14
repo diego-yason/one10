57aedefa75d33ef6c7852bafcc435073f0b6dfc3
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-06-15 00:41:07 +0800

    added first section of homepage

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-screen flex flex-col items-center justify-center">
+	<span class="text-4xl font-spaceGrotesk font-bold text-white">
+		<img src="https://placehold.co/60x50" alt="" class="inline" />
+		<span class="textOutline text-transparent">Studio</span> Lab
+	</span>
+	<h1 class="font-bold font-spaceGrotesk text-7xl text-white">We process your vision</h1>
+</div>
