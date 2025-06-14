ebf7100216ef8985c8c93575ec375dcd143e6730
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-06-14 17:59:09 +0800

    added nav bar

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -2,4 +2,20 @@
 	let { children } = $props();
 </script>
 
+<nav class="flex justify-between py-10 px-32 font-poppins">
+	<div class="flex flex-row gap-20 items-center">
+		<a href="/">
+			<img src="https://placehold.co/100x60" alt="" />
+		</a>
+		<a href="#">Home</a>
+		<a href="#">Services</a>
+		<a href="#">Track</a>
+		<a href="#">FAQs</a>
+	</div>
+	<div class="flex flex-row gap-14 items-center">
+		<a href="#">Cart</a>
+		<a href="#">Log In</a>
+	</div>
+</nav>
+
 {@render children()}
