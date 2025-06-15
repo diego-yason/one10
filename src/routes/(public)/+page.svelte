d964e8fba5d2c0bdebc70e9f170433e8f8de0414
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-06-16 01:20:39 +0800

    added panel 2

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -5,3 +5,47 @@
 	</span>
 	<h1 class="font-bold font-spaceGrotesk text-7xl text-white">We process your vision</h1>
 </div>
+<div class="px-40 bg-gray-800 py-20">
+	<div class="flex flex-row justify-around gap-40 mb-28">
+		<div class="grow">
+			<h2 class="text-amber-300 uppercase font-spaceGrotesk text-sm">--- Our Mission</h2>
+			<p class="font-spaceGrotesk text-4xl text-white font-bold">
+				Photography leads us to more interpretations of reality.
+			</p>
+		</div>
+		<p class="font-roboto text-white basis-1/2">
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam perspiciatis natus eaque,
+			suscipit, repellendus quos ad minus perferendis laborum praesentium repellat necessitatibus ut
+			accusantium consequuntur consequatur facere. Magni, voluptatem voluptatum?
+		</p>
+	</div>
+	<div class="flex gap-24">
+		<div>
+			<img src="https://placehold.co/72" alt="" class="mb-8" />
+			<p class="font-spaceGrotesk text-2xl text-white font-bold">Develop + Scan</p>
+			<p class="mt-5 mb-8">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, ducimus. Sit animi
+				fugiat fuga mollitia! Voluptates.
+			</p>
+			<a href="#" class="bg-amber-300 rounded-4xl px-6 py-2">Send us your film!</a>
+		</div>
+		<div>
+			<img src="https://placehold.co/72" alt="" class="mb-8" />
+			<p class="font-spaceGrotesk text-2xl text-white font-bold">Printing</p>
+			<p class="mt-5 mb-8">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore est exercitationem velit
+				earum?
+			</p>
+			<a href="#" class="bg-amber-300 rounded-4xl px-6 py-2">Print your work</a>
+		</div>
+		<div>
+			<img src="https://placehold.co/72" alt="" class="mb-8" />
+			<p class="font-spaceGrotesk text-2xl text-white font-bold">Studio Rental</p>
+			<p class="mt-5 mb-8">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, at! Officiis unde
+				reprehenderit blanditiis optio.
+			</p>
+			<p class="text-amber-300">Coming soon</p>
+		</div>
+	</div>
+</div>
