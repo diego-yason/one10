a8fdbb6b4a54f0155286f60f608af4cf000f41fc
zachfrancisco <zachary_francisco@dlsu.edu.ph>
2025-06-17 20:55:59 +0800

    Cart UI Added

---
+++ src/routes/(public)/cart/+page.svelte
@@ -0,0 +1,37 @@
+<div class="flex-col items-center justify-center text-center space-y-6 m-10">
+    <!--Item container-->
+    <h2 class="text-gray-800 font-spaceGrotesk text-7xl font-bold text-left mx-auto w-3/4 mb-auto h-30">Cart</h2>
+    <div class="flex bg-[#D9D9D9] p-4 rounded-lg mt-4 mx-auto w-3/4">
+        <div class="flex">
+            <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="" class="w-1/4 rounded-lg m-5">
+            <div class="flex-col text-left space-y-35">
+                <div>
+                    <h6 class="m-5 font-spaceGrotesk font-bold">Disposable Camera Service</h6>
+                    <p class="m-5">Film Format: 35mm</p>
+                </div>
+                <div>
+                    <div class="flex border-1 w-20 m-5 p-1 rounded-lg justify-around">
+                        <button>-</button>
+                        <p>1</p>
+                        <button>+</button>
+                    </div>
+                    <h6 class="text-left m-5 font-bold">P250</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Order Summary-->
+    <div class="flex border-1 rounded-lg mx-auto w-3/4 my-auto y-100 p-5 text-left font-bold">
+        <div>
+            <h2>Order Summary</h2>
+            <div class="flex space-x-4">
+                <h3>Total</h3>
+                <h3 class="font-normal">P500</h3>
+            </div>
+        </div>
+        <div class="ml-auto">
+            <button class="bg-yellow-400 text-white py-2 px-4 rounded-lg hover:bg-amber-700 transition duration-300">Checkout</button>
+        </div>
+    </div>
+</div>
