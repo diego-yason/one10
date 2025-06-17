34a7af4425c01a76a84eddb658f871dff4aafeec
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-06-17 21:08:45 +0800

    modified zach UI

--- src/routes/(public)/cart/+page.svelte
+++ src/routes/(public)/cart/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import CartItem from './CartItem.svelte';
+</script>
+
 <div class="flex-col items-center justify-center text-center space-y-6 m-10">
 	<!--Item container-->
 	<h2
@@ -7,25 +11,7 @@
 	</h2>
 	<div class="flex bg-[#D9D9D9] p-4 rounded-lg mt-4 mx-auto w-3/4">
 		<div class="flex">
-			<img
-				src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
-				alt=""
-				class="w-1/4 rounded-lg m-5"
-			/>
-			<div class="flex-col text-left space-y-35">
-				<div>
-					<h6 class="m-5 font-spaceGrotesk font-bold">Disposable Camera Service</h6>
-					<p class="m-5">Film Format: 35mm</p>
-				</div>
-				<div>
-					<div class="flex border-1 w-20 m-5 p-1 rounded-lg justify-around">
-						<button>-</button>
-						<p>1</p>
-						<button>+</button>
-					</div>
-					<h6 class="text-left m-5 font-bold">P250</h6>
-				</div>
-			</div>
+			<CartItem></CartItem>
 		</div>
 	</div>
 
---
+++ src/routes/(public)/cart/CartItem.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let count = $state(1);
+</script>
+
+<img
+	src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
+	alt=""
+	class="w-1/4 rounded-lg m-5"
+/>
+<div class="flex-col text-left space-y-35">
+	<div>
+		<h6 class="m-5 font-spaceGrotesk font-bold">Disposable Camera Service</h6>
+		<p class="m-5">Film Format: 35mm</p>
+	</div>
+	<div>
+		<div class="flex border-1 w-20 m-5 rounded-lg justify-around">
+			<button
+				class="px-1.5 basis-1/4 py-3 border-r-[1px] border-black"
+				onclick={() => count--}
+				disabled={count <= 0}>-</button
+			>
+			<span class="grow flex items-center justify-center">{count}</span>
+			<button class="px-1.5 basis-1/4 py-3 border-l-[1px] border-black" onclick={() => count++}
+				>+</button
+			>
+		</div>
+		<h6 class="text-left m-5 font-bold">P250</h6>
+	</div>
+</div>
