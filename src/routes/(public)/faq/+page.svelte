1e130e5d769af68fee357847a9eeb7124bcbd872
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-06-17 17:44:13 +0800

    did some faq stuffs

---
+++ src/routes/(public)/faq/+page.svelte
@@ -0,0 +1,21 @@
+<h2 class="font-spaceGrotesk font-bold text-7xl text-center text-gray-[#333] mt-20 mb-16">
+	Frequently Asked Questions
+</h2>
+<div class="flex flex-col gap-7 w-lg rounded-2xl mx-auto">
+	<div class="flex justify-between bg-white p-3.5">
+		<p>How do I place an order?</p>
+		<span>+</span>
+	</div>
+	<div class="flex justify-between bg-white p-3.5">
+		<p>How do I place an order?</p>
+		<span>+</span>
+	</div>
+	<div class="flex justify-between bg-white p-3.5">
+		<p>How do I place an order?</p>
+		<span>+</span>
+	</div>
+	<div class="flex justify-between bg-white p-3.5">
+		<p>How do I place an order?</p>
+		<span>+</span>
+	</div>
+</div>
