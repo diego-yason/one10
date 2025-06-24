028ba182062c0c6ceee9aab17ead963e9df8f12c
Jedidiah Julian <charlesxxavier30@gmail.com>
2025-06-24 17:38:02 +0800

    feat: finished manage products page

---
+++ src/routes/(staff)/products/+page.svelte
@@ -0,0 +1,42 @@
+<div class="flex space-y-6 flex-col">
+    <input type="text" placeholder="Search Product Name" class="px-5 py-3 rounded-lg bg-white border-0 w-110">
+    <p class="font-bold"> Products: </p>
+</div>
+
+<div class="flex justify-between mt-10">
+    <div class="flex flex-col bg-white h-100 w-70 shadow-2xl rounded-3xl items-center hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+        <div class="h-50 w-full bg-gray-100 object-cover flex items-center justify-center rounded-t-3xl">Image placeholder</div>
+
+        <div class="flex flex-col justify-center items-center space-y-10 mt-10">
+            <p class="font-bold text-xl">Product 1</p>    
+            <button class="border px-5 py-2 rounded-full hover:cursor-pointer hover:bg-black hover:text-white">Manage</button>
+        </div>
+    </div>
+
+    <div class="flex flex-col bg-white h-100 w-70 shadow-2xl rounded-3xl items-center hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+        <div class="h-50 w-full bg-gray-100 object-cover flex items-center justify-center rounded-t-3xl">Image placeholder</div>
+
+        <div class="flex flex-col justify-center items-center space-y-10 mt-10">
+            <p class="font-bold text-xl">Product 2</p>    
+            <button class="border px-5 py-2 rounded-full hover:cursor-pointer hover:bg-black hover:text-white">Manage</button>
+        </div>
+    </div>
+
+    <div class="flex flex-col bg-white h-100 w-70 shadow-2xl rounded-3xl items-center hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+        <div class="h-50 w-full bg-gray-100 object-cover flex items-center justify-center rounded-t-3xl">Image placeholder</div>
+
+        <div class="flex flex-col justify-center items-center space-y-10 mt-10">
+            <p class="font-bold text-xl">Product 3</p>    
+            <button class="border px-5 py-2 rounded-full hover:cursor-pointer hover:bg-black hover:text-white">Manage</button>
+        </div>
+    </div>
+
+    <div class="flex flex-col bg-white h-100 w-70 shadow-2xl rounded-3xl items-center hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+        <div class="h-50 w-full bg-gray-100 object-cover flex items-center justify-center rounded-t-3xl">Image placeholder</div>
+
+        <div class="flex flex-col justify-center items-center space-y-10 mt-10">
+            <p class="font-bold text-xl">Product 4</p>    
+            <button class="border px-5 py-2 rounded-full hover:cursor-pointer hover:bg-black hover:text-white">Manage</button>
+        </div>
+    </div>
+</div>
