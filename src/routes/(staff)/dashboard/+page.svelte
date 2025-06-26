5f01374b70c6ae112c2781030f67be3260190f68
shampeyndadi <152191672+shampeyndadi@users.noreply.github.com>
2025-06-26 21:22:09 +0800

    [FEAT] Staff design implementation (#18)
    * 1st push
    
    * added layout for staff
    
    * feat: orders page
    
    * Update +page.svelte
    
    * feat: finished dashboard
    
    * feat: finished manage products page
    
    * Delete forgot-password
    
    * delete
    
    * feat: manage products page done
    
    ---------
    
    Co-authored-by: Lance Reyes <lr.lancereyes@gmail.com>
    Co-authored-by: Matthew Corral <114600832+chutipie@users.noreply.github.com>
    Co-authored-by: diego-yason <61825575+diego-yason@users.noreply.github.com>

---
+++ src/routes/(staff)/dashboard/+page.svelte
@@ -0,0 +1,32 @@
+<div class="flex flex-col">
+    <div class="flex flex-col space-y-6">
+        <p class="font-bold text-2xl flex justify-center">Good day! <br> Employee</p>
+        <div class="flex justify-center">
+            <div class="bg-white flex justify-center items-center w-60 h-60 rounded-full shadow-2xl"> Image Placeholder </div>
+        </div>
+
+        <div class="flex justify-center items-center">
+            <p class="font-bold">Today we have:</p>
+        </div>
+
+        <div class="flex justify-evenly">
+            <div class="flex flex-col bg-white h-100 w-60 shadow-2xl rounded-3xl items-center space-y-10 hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+                <div class="flex justify-center w-30 h-30 rounded-full bg-yellow-300 mt-10"></div>
+
+                <p class="font-bold py-10">12 pending orders</p>
+            </div>
+
+            <div class="flex flex-col bg-white h-100 w-60 shadow-2xl rounded-3xl items-center space-y-10 hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+                <div class="flex justify-center w-30 h-30 rounded-full bg-green-400 mt-10"></div>
+
+                <p class="font-bold py-10">3 newly placed orders</p>
+            </div>
+
+            <div class="flex flex-col bg-white h-100 w-60 shadow-2xl rounded-3xl items-center space-y-10 hover:cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-105">
+                <div class="flex justify-center w-30 h-30 rounded-full bg-blue-400 mt-10"></div>
+
+                <p class="font-bold py-10">10 files ready for upload</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/routes/(staff)/orders/+page.svelte
@@ -0,0 +1,59 @@
+<input type="text" placeholder="Search Order Number or Customer Name" class="px-5 py-3 rounded-lg bg-white border-0 w-110">
+
+<div class="flex flex-col mt-8">
+    <div class="flex mb-5 justify-between">
+        <p class="font-bold text-xl">Orders:</p>
+
+        <button class="font-bold rounded-2xl bg-white px-7 hover:cursor-pointer hover:bg-black hover:text-white">Filter</button>
+    </div>
+
+    <div class="flex flex-col space-y-11">
+        <div class="flex flex-col space-y-6 bg-white px-5 py-3 rounded-2xl">
+            <div class="flex justify-between">
+                <p class="font-bold">Order #23341</p>
+
+                <p class="flex items-center gap-3">Pending <span class="bg-yellow-300 rounded-3xl w-5 h-5"></span></p>
+            </div>
+
+            <p>Customer Name: <span class="font-bold">Juan Dela Cruz</span></p>
+
+            <div class="flex justify-between">
+                <p>Order placed: <span class="font-bold">06/16/2025</span></p>
+
+                <button class="bg-black rounded-lg font-color text-white py-2 px-6 hover:cursor-pointer hover:shadow-2xl">View Details</button>
+            </div>
+        </div>
+
+        <div class="flex flex-col space-y-6 bg-white px-5 py-3 rounded-2xl">
+            <div class="flex justify-between">
+                <p class="font-bold">Order #253121</p>
+
+                <p class="flex items-center gap-3">For pickup/delivery <span class="bg-green-600 rounded-3xl w-5 h-5"></span></p>
+            </div>
+
+            <p>Customer Name: <span class="font-bold">Peter Parker</span></p>
+
+            <div class="flex justify-between">
+                <p>Order placed: <span class="font-bold">06/10/2025</span></p>
+
+                <button class="bg-black rounded-lg font-color text-white py-2 px-6 hover:cursor-pointer hover:shadow-2xl">View Details</button>
+            </div>
+        </div>
+
+        <div class="flex flex-col space-y-6 bg-white px-5 py-3 rounded-2xl">
+            <div class="flex justify-between">
+                <p class="font-bold">Order #265317</p>
+
+                <p class="flex items-center gap-3">In progress <span class="bg-blue-600 rounded-3xl w-5 h-5"></span></p>
+            </div>
+
+            <p>Customer Name: <span class="font-bold">Barry Allen</span></p>
+
+            <div class="flex justify-between">
+                <p>Order placed: <span class="font-bold">06/17/2025</span></p>
+
+                <button class="bg-black rounded-lg font-color text-white py-2 px-6 hover:cursor-pointer hover:shadow-2xl">View Details</button>
+            </div>
+        </div>
+    </div>
+</div>
