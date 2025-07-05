bf02af53426460bcd049e6d010b71d023d3f33cd
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-07-05 21:00:48 +0800

    started on protecting routes

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,14 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user:
+				| {
+						id: string;
+						email: string | null;
+				  }
+				| { id: null; email: null };
+		}
 		// interface PageData {}
 		// interface PageState {}
 		interface Platform {
---
+++ src/hooks.server.ts
@@ -0,0 +1,42 @@
+import { type Handle, redirect } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+
+import { auth } from '$lib/firebase';
+
+const user: Handle = async ({ event, resolve }) => {
+	if (!auth.currentUser) event.locals.user = { email: null, id: null };
+	else {
+		const { email, uid } = auth.currentUser;
+		event.locals.user = {
+			email,
+			id: uid
+		};
+	}
+
+	return resolve(event);
+};
+
+const authGuardStaff: Handle = async ({ event, resolve }) => {
+	const staffRoutes = ['/dashboard', '/messages', '/orders', '/products'];
+	if (staffRoutes.some((route) => event.url.pathname.startsWith(route)))
+		if (!event.locals.user.email?.endsWith('@one10studiolab.com'))
+			// TODO: confirm if 3XX is correct
+			return redirect(307, '/');
+	return resolve(event);
+};
+
+const authGuardUser: Handle = async ({ event, resolve }) => {
+	// TODO: determine what public routes need protection
+	const signedInRoutes = ['/profile']; // example
+	const signedOutRoutes = ['/login', '/register'];
+
+	if (signedInRoutes.some((route) => event.url.pathname.startsWith(route)))
+		if (event.locals.user.id == null) return redirect(307, '/login');
+
+	if (signedOutRoutes.some((route) => event.url.pathname.startsWith(route)))
+		if (event.locals.user.id == null) return redirect(307, '/');
+
+	return resolve(event);
+};
+
+export const handle = sequence(user, authGuardStaff, authGuardUser);
