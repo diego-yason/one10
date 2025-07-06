ba4520e967a4160564fab79988d974ac3bc854d8
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-07-07 00:26:38 +0800

    added test IDs as per test docs

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('user can login on /login page', async ({ page }) => {
+test('[1-A] user can login on /login page', async ({ page }) => {
 	// test.setTimeout(); // Extend to handle Firebase delays
 
 	await page.goto('/login');
@@ -12,12 +12,27 @@ test('user can login on /login page', async ({ page }) => {
 
 	// Click the "Login" button
 	await page.getByText('Login', { exact: true }).click();
-	console.log('Login button clicked');
 
 	// Assert that we are redirected to the home page
 	await expect(page).toHaveURL('/');
-	console.log('Redirected to home page');
 
 	// Assert "Sign Out" button is visible
 	await expect(page.getByText('Sign Out', { exact: true })).toBeVisible();
+
+	// visit profile page
+	// TODO
+	// await page.screenshot({ path: "test-results/1-A.png", fullPage: true });
+});
+
+test('[1-B] invalid password', async ({ page }) => {
+	await page.goto('/login');
+
+	// Fill in login credentials
+	await page.getByRole('textbox', { name: 'email' }).fill('test@one10studiolab.com');
+	await page.getByRole('textbox', { name: 'password' }).fill('wrongpassword');
+	await page.getByText('Login', { exact: true }).click();
+
+	await expect(page.getByText('Incorrect password.', { exact: true })).toBeVisible();
+
+	await page.screenshot({ path: 'test-results/1-B.png', fullPage: true });
 });
