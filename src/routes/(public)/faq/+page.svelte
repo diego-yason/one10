8e1eb5d46f719ac15dece42620147fb80f13477b
shampeyndadi <charlesxxavier30@gmail.com>
2025-07-12 18:57:11 +0800

    feat: faq implemented

--- src/routes/(public)/faq/+page.svelte
+++ src/routes/(public)/faq/+page.svelte
@@ -2,71 +2,62 @@
     <h1 class="title">Frequently Asked Questions</h1>
 
     <div class="flex flex-col space-y-5">
-        <div class="container bg-white flex items-center justify-between">
-            <p class="font-style">How do I place an order?</p>
-            <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none">
-                    <g clip-path="url(#clip0_1018_92)">
-                    <path d="M15 6.4375H9.0625V0.5H5.9375V6.4375H0V9.5625H5.9375V15.5H9.0625V9.5625H15V6.4375Z" fill="#6C5CCC"/>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_1018_92">
-                    <rect width="15" height="15" fill="white" transform="translate(0 0.5)"/>
-                    </clipPath>
-                    </defs>
-                </svg>
-            </span>
-        </div>
+        {#each faqs as faq, i}
+            <div class="container bg-white" on:click={() => open(i)}>
+                <div class="flex items-center justify-between">
+                    <p class="font-style">{faq.question}?</p>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none" class="hover:cursor-pointer">
+                            <g clip-path="url(#clip0_1018_92)">
+                            <path d="M15 6.4375H9.0625V0.5H5.9375V6.4375H0V9.5625H5.9375V15.5H9.0625V9.5625H15V6.4375Z" fill="#6C5CCC"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_1018_92">
+                            <rect width="15" height="15" fill="white" transform="translate(0 0.5)"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                    </span>
+                </div>
 
-        <div class="container bg-white flex items-center justify-between">
-            <p class="font-style">How do I pay for my order?</p>
-            <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none">
-                    <g clip-path="url(#clip0_1018_92)">
-                    <path d="M15 6.4375H9.0625V0.5H5.9375V6.4375H0V9.5625H5.9375V15.5H9.0625V9.5625H15V6.4375Z" fill="#6C5CCC"/>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_1018_92">
-                    <rect width="15" height="15" fill="white" transform="translate(0 0.5)"/>
-                    </clipPath>
-                    </defs>
-                </svg>
-            </span>
-        </div>
-
-        <div class="container bg-white flex items-center justify-between">
-            <p class="font-style">How do I send my film?</p>
-            <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none">
-                    <g clip-path="url(#clip0_1018_92)">
-                    <path d="M15 6.4375H9.0625V0.5H5.9375V6.4375H0V9.5625H5.9375V15.5H9.0625V9.5625H15V6.4375Z" fill="#6C5CCC"/>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_1018_92">
-                    <rect width="15" height="15" fill="white" transform="translate(0 0.5)"/>
-                    </clipPath>
-                    </defs>
-                </svg>
-            </span>
-        </div>
-
-        <div class="container bg-white flex items-center justify-between">
-            <p class="font-style">How long does the process take?</p>
-            <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16" fill="none">
-                    <g clip-path="url(#clip0_1018_92)">
-                    <path d="M15 6.4375H9.0625V0.5H5.9375V6.4375H0V9.5625H5.9375V15.5H9.0625V9.5625H15V6.4375Z" fill="#6C5CCC"/>
-                    </g>
-                    <defs>
-                    <clipPath id="clip0_1018_92">
-                    <rect width="15" height="15" fill="white" transform="translate(0 0.5)"/>
-                    </clipPath>
-                    </defs>
-                </svg>
-            </span>
-        </div>
+                {#if faq.open}
+				    <p class="mt-5 text-gray-600" transition:slide>{faq.answer}</p>
+			    {/if}
+            </div>
+        {/each}
     </div>
 </div>
+
+<script lang="ts">
+    import { slide } from 'svelte/transition';
+
+    let faqs = [
+        {
+            question: "How do I place an order?",
+            answer: "blabla",
+            open: false
+        },
+         {
+            question: "How do I pay for my order?",
+            answer: "",
+            open: false
+        },
+         {
+            question: "How do I send my film?",
+            answer: "",
+            open: false
+        },
+         {
+            question: "How long does the process take?",
+            answer: "",
+            open: false
+        },
+    ];
+
+    function open(index: number){
+        faqs[index].open = !faqs[index].open
+    }
+</script>
 
 <style>
     .title{
