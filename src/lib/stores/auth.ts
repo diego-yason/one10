5149f59ecba9cd7634e4150a72f22ffc3521a40d
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-07-12 21:53:11 +0800

    added invalidation for loading

--- src/lib/stores/auth.ts
+++ src/lib/stores/auth.ts
@@ -2,6 +2,7 @@ import { writable } from 'svelte/store';
 import { auth } from '$lib/services/firebase';
 import { onAuthStateChanged, type User } from 'firebase/auth';
 import { browser } from '$app/environment';
+import { invalidate } from '$app/navigation';
 
 export const user = writable<User | null>(null);
 export const isStaff = writable(false);
@@ -9,8 +10,20 @@ export const authLoaded = writable(false);
 
 user.subscribe((user) => isStaff.set(user?.email?.endsWith('@one10studiolab.com') ?? false));
 
+let userStatic: User | null | undefined = undefined;
+user.subscribe((u) => (userStatic = u));
+let authLoadedStatic = false;
+authLoaded.subscribe((loaded) => (authLoadedStatic = loaded));
+
 if (browser) {
 	onAuthStateChanged(auth, (newUser) => {
+		// invalidate if authLoaded and userStatic was a user and now is null
+		// aka a logout
+		if (authLoadedStatic && userStatic && !newUser) {
+			console.warn("User logged out, invalidating 'user'");
+			invalidate('auth:user');
+		}
+
 		user.set(newUser);
 		authLoaded.set(true);
 	});
