6c3d8c2979d91cf11647e368778fee69e2b4d241
zachfrancisco <165399471+zachfrancisco@users.noreply.github.com>
2025-07-16 20:37:41 +0800

    [FEAT] Track page (#63)
    [FEAT] [Zach] Track page (no functionality)
    
    Co-authored-by: Diego Yason <61825575+diego-yason@users.noreply.github.com>
    Co-authored-by: Lance Reyes <lr.lancereyes@gmail.com>

---
+++ src/routes/(public)/track/+page.svelte
@@ -0,0 +1,102 @@
+<div class="flex-col pt-40 mx-40 text-[#333] mb-20">
+	<h1 class="text-7xl font-bold font-spaceGrotesk">Track</h1>
+	<!--Order container-->
+	<div class="bg-[#F2C94C] text-black p-8 rounded-2xl my-10 shadow-xl">
+		<div class="flex place-content-between">
+			<h2 class="text-4xl pb-2">Develop + Scanning</h2>
+			<button class="bg-gray-300 rounded-xl px-8 shadow-xl">Edit</button>
+		</div>
+		<p>Order Number: 12345678</p>
+		<p>Negatives will not be returned</p>
+		<div class="flex m-10 mb-5 items-center justify-center">
+			<div class="w-15 h-15 border-6 rounded-full"></div>
+			<div class="w-75 h-1 bg-black"></div>
+			<div class="w-15 h-15 bg-black rounded-full"></div>
+			<div class="w-75 h-1 bg-black"></div>
+			<div class="w-15 h-15 border-6 rounded-full"></div>
+		</div>
+		<div class="flex place-content-around text-center">
+			<div class="w-60">
+				<p>Waiting for Drop-off at Lab</p>
+			</div>
+			<div class="w-60">
+				<p>Processing Films</p>
+			</div>
+			<div class="w-60">
+				<p>Uploaded to Google Drive</p>
+			</div>
+		</div>
+		<div class="flex place-content-around text-center text-xs">
+			<div class="w-60">
+				<p>Chosen Drop-off point: Sta. Ana, Manila</p>
+			</div>
+			<div class="w-60">
+				<p></p>
+			</div>
+			<div class="w-60">
+				<p></p>
+			</div>
+		</div>
+	</div>
+	<!--Order Container-->
+	<div class="bg-gray-300 rounded-2xl">
+		<div class="bg-[#F2C94C] text-black p-8 rounded-2xl mt-10 shadow-xl">
+			<div class="flex place-content-between">
+				<h2 class="text-4xl pb-2">Develop + Scanning</h2>
+				<button class="bg-gray-300 rounded-xl px-8 shadow-xl">Edit</button>
+			</div>
+			<p>Order Number: 12345678</p>
+			<p>Negatives will not be returned</p>
+			<div class="flex m-10 mb-5 items-center justify-center">
+				<div class="w-15 h-15 border-6 rounded-full"></div>
+				<div class="w-60 h-1 bg-black"></div>
+				<div class="w-15 h-15 bg-black rounded-full"></div>
+				<div class="w-60 h-1 bg-black"></div>
+				<div class="w-15 h-15 border-6 rounded-full"></div>
+				<div class="w-60 h-1 bg-black"></div>
+				<div class="w-15 h-15 border-6 rounded-full"></div>
+			</div>
+			<div class="flex place-content-around text-center">
+				<div class="w-60">
+					<p>Waiting for Drop-off at Lab</p>
+				</div>
+				<div class="w-60">
+					<p>Processing Films</p>
+				</div>
+				<div class="w-60">
+					<p>Uploaded to Google Drive</p>
+				</div>
+				<div class="w-60">
+					<p>Retrieve Negatives</p>
+				</div>
+			</div>
+			<div class="flex place-content-around text-center text-xs">
+				<div class="w-60">
+					<p>Chosen Drop-off point: Sta. Ana, Manila</p>
+				</div>
+				<div class="w-60">
+					<p></p>
+				</div>
+				<div class="w-60">
+					<p></p>
+				</div>
+				<div class="w-60">
+					<p></p>
+				</div>
+			</div>
+		</div>
+        <div>
+            <form action="">
+                <div class="flex flex-col">
+                    <input type="text" placeholder="Type your film's brand name" class="p-5 rounded-sm text-black border-1 min-w-100 max-w-150 m-8 mb-2" />
+                    <select class="p-5 rounded-sm text-black border-1 min-w-100 max-w-150 m-8 mb-2">
+                        <option disabled selected>Method of delivery</option>
+                        <option>Method 1</option>
+                        <option>Method 2</option>
+                    </select>
+                    <input type="text" placeholder="Return shipping address" class="p-5 rounded-sm text-black border-1 min-w-100 max-w-150 m-8" />
+                </div>
+            </form>
+        </div>
+	</div>
+</div>
