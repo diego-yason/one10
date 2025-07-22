6143f7d376ff050f9d6141cecd6ac55e55dec342
diego-yason <61825575+diego-yason@users.noreply.github.com>
2025-07-22 20:41:00 +0800

    added some config changes

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,20 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				persist: './wrangler-state'
+			}
+		}),
 		alias: {
 			$types: './types'
+		},
+		env: {
+			publicPrefix: 'PUBLIC_'
 		}
 	}
 };
