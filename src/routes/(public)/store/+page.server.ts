ee30889968eb46d5b18fdae8cd26785dcccfda0e
Roan Cedric Campo <roan_campo@dlsu.edu.ph>
2025-12-05 07:00:31 +0800

    Quick fix
    Note: Newly added items uploaded to the DB must contain a 'createdAt' and 'updatedAt' field. Else, the app breaks whenever you go to cart. This small fix prevents that.

--- src/routes/(public)/store/+page.server.ts
+++ src/routes/(public)/store/+page.server.ts
@@ -1,15 +1,15 @@
 import { adminDb } from '$lib/server/firebase';
 import type { FirebaseProduct } from '$types/firebase/Products';
-import type { PageServerLoad } from './$types';
 import type { QueryDocumentSnapshot } from 'firebase-admin/firestore';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const products = (
 		(await adminDb.collection('products').get()).docs as QueryDocumentSnapshot<FirebaseProduct>[]
 	).map((doc) => ({
 		...doc.data(),
-		createdAt: doc.data().createdAt.seconds,
-		updatedAt: doc.data().updatedAt.seconds,
+		createdAt: doc.data().createdAt?.seconds ?? 0,
+		updatedAt: doc.data().updatedAt?.seconds ?? 0,
 		id: doc.id
 	}));
 
